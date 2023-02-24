eeda795a8eaa35aca669da920eadb68cadc3a2f3
sunil mondal <mondalsunil415@gmail.com>
2023-02-24 22:42:23 +0530

    ex4

---
+++ ex4.py
@@ -0,0 +1,11 @@
+tom_exp_list = [2100, 3400, 5600]
+joe_exp_list = [2300, 2540, 1560]
+total = 0
+for item in tom_exp_list:
+    total = total+item
+print("tom's total expenses:", total)
+
+total = 0
+for item in joe_exp_list:
+    total = total+item
+print("joe's total expenses:", total)
